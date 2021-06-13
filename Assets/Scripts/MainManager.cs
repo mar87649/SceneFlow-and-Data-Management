2c428e55ceea466ff2e53dd243a389b2dbb3d960
Mario Martinez <43589857+mar87649@users.noreply.github.com>
2021-06-13 13:27:00 -0500

    late commit

---
+++ Assets/Scripts/MainManager.cs
@@ -0,0 +1,48 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System.IO;
+
+public class MainManager : MonoBehaviour
+{
+    //THIS IS A SINGLETON PATTERN
+    public static MainManager Instance {get; private set;}
+    public Color TeamColor;
+
+
+    private void Awake() {
+        LoadColor();
+        if (Instance != null)
+        {
+            Destroy(gameObject);
+            return;
+        }
+        Instance = this;
+        DontDestroyOnLoad(gameObject);    
+    }
+    [System.Serializable]
+    class SaveData
+    {
+        public Color TeamColor;
+    }
+    public void SaveColor()
+    {
+        SaveData data = new SaveData();
+        data.TeamColor = TeamColor;
+
+        string json = JsonUtility.ToJson(data);
+        //use system.io to save data to path as a json file
+        File.WriteAllText(Application.persistentDataPath + "/savefile.json", json);
+    }
+
+    public void LoadColor()
+    {
+        string path = Application.persistentDataPath + "/savefile.json";
+        if (File.Exists(path))
+        {
+            string json = File.ReadAllText(path);
+            SaveData data = JsonUtility.FromJson<SaveData>(json);
+            TeamColor = data.TeamColor;
+        }
+    }
+}
